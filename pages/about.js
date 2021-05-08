11a330dd56bfbbe1052e23abe59e93cee937d0a5
sakamotodd <73228728+sakamotodd@users.noreply.github.com>
2021-05-08 11:57:41 +0900

    ✨ ヘッダーの追加

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import{Main} from '../components/Main';
 import {Footer} from '../components/Footer'
+import { Header } from '../components/Header';
 
 export default function about() {
   return (
     <div className={styles.container}>
+      <Header />
       <Head>
         <title>Index page</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,5 +15,4 @@ export default function about() {
       <Main page="about" />
       <Footer/>
     </div>
-  );
-}
+  );}

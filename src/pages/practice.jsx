856004aaac4b4d94a0a1c88758eb45e876c5c05c
sakamotodd <73228728+sakamotodd@users.noreply.github.com>
2021-05-25 06:34:20 +0900

    ✨ 追加機能、削除機能実装

--- src/pages/practice.jsx
+++ src/pages/practice.jsx
@@ -2,33 +2,54 @@ import { Header } from '../components/Header';
 import React, { useState } from 'react';
 
 export default function practice() {
-  //   const defaultValue = { title: '', doing: false };
-  const [tasks, setTasks] = useState([]);
-  const [tmp, setTmp] = useState();
+  const [str, setStr] = useState([]);
+  const [tmp, setTmp] = useState('');
+  const [findTodo, setFindTodo] = useState([]);
 
-  const addTask = () => {
-    setTasks([...tasks, tmp]);
+  const arr = ['aaa', 'bbb', 'ccc'];
+  const val = 'bbb';
+  const b = arr.indexOf(val);
+  console.log(b);
+
+  const todoChange = (e) => {
+    setTmp(e.target.value);
+  };
+
+  const addTodo = () => {
+    setStr([...str, tmp]);
     setTmp('');
   };
 
-  const deleteTask = () => {
-    setTmp('');
+  const removeTodo = (s) => {
+    setStr(str.filter((x) => x !== s));
   };
-    
+
+  useEffect(() => {
+    const newItems = str.filter((xx) => {
+      return xx.indexOf(value) !== -1;
+    });
+    setFindTodo(newItems);
+  }, [value]);
+
   return (
-    <>
+    <div>
       <Header />
-      <input type="text" value={tmp} onChange={(e) => setTmp(e.target.value)} />
-      <button onClick={addTask}>追加</button>
-      {tasks.map((task) => {
+
+      <input type="text" value={tmp} onChange={todoChange} />
+      <input type="text" value={tmp} onChange={todoChange} />
+
+      <button onClick={addTodo}>ボタン</button>
+      {str.map((s, index) => {
         return (
-          <div key={task}>
-            <div>{task}
-            <button>削除</button>
-            </div>
-          </div>
+          <ul key={index}>
+            <li>
+              {s}
+              <button onClick={() => removeTodo(s)}>削除</button>
+              <button onClick={() => update(s)}>更新</button>
+            </li>
+          </ul>
         );
       })}
-    </>
+    </div>
   );
 }
